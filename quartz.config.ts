73c9df529e3d6dff6699f944795f7ee4195be428
TunaZalad <trashcan0908@gmail.com>
2025-10-26 03:47:03 -0500

    Quartz sync: Oct 26, 2025, 3:47 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Secular One",
+        body: "Playfair Display",
         code: "IBM Plex Mono",
       },
       colors: {
